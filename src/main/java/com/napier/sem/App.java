974e8e390c79f8fa41a2b1e30f3730964181c618
Devyon Lozano <devyonlozano@gmail.com>
2025-10-10 18:21:44 -0400

    Insert document in mongodb

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,29 @@
 package com.napier.sem;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Boo yah!");
+        // Connect to MongoDB on local system using port 27000
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27000");
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store
+        Document document = new Document("name", "Devyon Lozano")
+                .append("class", "DevOps")
+                .append("year", "2025")
+                .append("result", new Document("CW", 95).append("EX", 85));
+        // Add document to collection
+        collection.insertOne(document);
+
+        // Check document in collection
+        Document myDocument = collection.find().first();
+        System.out.println(myDocument.toJson());
     }
 }
